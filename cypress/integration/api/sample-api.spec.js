47d2d3b764892aa329fc027465c9eab07f3a3dcd
esteban.candia <esteban.candia@endava.com>
2022-05-03 13:47:03 -0300

    init project

---
+++ cypress/integration/api/sample-api.spec.js
@@ -0,0 +1,14 @@
+
+describe('Example API Test', () => {
+    beforeEach(() => {
+
+    })
+  
+    it('Example API Test 1', () => {
+        //TODO
+    })
+
+    it('Example API Test 2', () => {
+        //TODO
+    })
+})
---
+++ cypress/integration/e2e/sample-e2e.spec.js
@@ -0,0 +1,14 @@
+
+describe('Example E2E Test', () => {
+    beforeEach(() => {
+
+    })
+  
+    it('Example E2E Test 1', () => {
+        //TODO
+    })
+
+    it('Example E2E Test 2', () => {
+        //TODO
+    })
+})
