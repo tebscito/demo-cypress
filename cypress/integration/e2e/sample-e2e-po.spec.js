5b428a3aa33fb0ff3402c6fd13ae5289889d1a95
Maria Cecilia Sangineto Ruibal <mariacecilia.sanginetoruibal@endava.com>
2022-05-10 13:21:16 -0300

    pom

--- cypress/integration/e2e/sample-e2e-po.spec.js
+++ cypress/integration/e2e/sample-e2e-po.spec.js
@@ -1,60 +1,51 @@
-import characters from '../../support/page-object/characters';
+import Character from '../../support/page-object/Character';
 import card from '../../fixtures/card.json'
-import selectors from '../../support/selectors'
-// Ruta desde este archivo hasta el archivo donde tengo mis clases (sin la extension .js)
 
 describe('Example E2E Test', () => {
-    let character = new characters();
+    let character = new Character();
 
     before(() => {
-        character.visitCharacters();
+        character.visitCharacters('https://dsternlicht.github.io/RESTool/#/characters?search=');
     })
 
     it('Example E2E Test 1 - Create new Character Card', () => {
-        character.addItemButton()
-        character.fillCharacter()
-
-            cy.get(selectors.SUCCESS_ALERT).should(($el) => {
-                expect($el).to.have.text('Great Success!')
-            })
-
-            cy.get(selectors.SUCCESS_ALERT).should(($el) => {
-                expect($el).not.to.exist
-            })
+        character.locator.addItem().click()
+        character.fillCharacter(card.image, card.name, card.real_name, card.current_location) // p
+        character.locator.successAlert().should(($el) => {
+            expect($el).to.have.text('Great Success!')
+        })
+        character.locator.successAlert().should(($el) => {
+            expect($el).not.to.exist
+        })
     })
 
     it('Example E2E Test 2 - Search an Existing Character', () => {
         character.wait(3000)
-
-        character.waitingAllCharacters()
-        character.typeName()
-        character.clickOnCharacterSubmitButton()
-
+        character.waitingAllCharacters() //p
+        character.locator.characterSearchInputText().type(card.name)
+        character.locator.characterSubmitButton().click()
         character.waitGetChar()
-        cy.get(selectors.CARD_ROW_TEXT).should(($el) => {
+        character.locator.cardRowText().should(($el) => {
             expect($el.eq(1)).to.have.text(card.name)
             expect($el.eq(2)).to.have.text(card.real_name)
             expect($el.eq(3)).to.have.text(card.current_location)
         })
-            .get(selectors.CARD_ROW_LABEL).should(($el) => {
-                expect($el).to.be.visible
-            })
+        character.locator.cardRowLabel().should(($el) => {
+            expect($el).to.be.visible
+        })
     })
 
     it('Example E2E Test 3 - Search and Delet a Character', () => {
         character.wait(3000)
-
-        character.waitingAllCharacters()
-        character.clearPreviousSearchAndWrite()
-        character.clickOnSearchSubmitButton()
-
-        cy.get(selectors.CARD).should('be.visible')
-        // cy  
-        //     .wait('@getChar')
-        //     .get(selectors.CARD).should('be.visible')
-        //     .get(selectors.DELETE_CARD).click()
-        //     .get(selectors.CARD_NOT_FOUND).should(($el) => {
-        //         expect($el).to.have.text('Nothing to see here. Result is empty.')
-        //     })
+        character.waitingAllCharacters() //p 
+        character.locator.characterSearchInputText().clear().type(card.name)
+        character.locator.characterSubmitButton().click()
+        character.waitGetChar()
+        character.locator.card().should('be.visible')
+        character.locator.deleteCard().click()
+        character.locator.cardNotFound().should(($el) => {
+            expect($el).to.have.text('Nothing to see here. Result is empty.')
+        })
     })
+
 })
